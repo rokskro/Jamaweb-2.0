5981d62d48776fa6b91216710a2987698f6711aa
rox <rokskro@gmail.com>
2024-02-19 09:53:43 +0000

    images

---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function ImageContainer(){
+    return(
+        <div className="container">
+        <div className="image">
+          <img src="fish-image.png" />
+        </div>
+        <div className="image">
+          <img src="senor-image.png" />
+        </div>
+        <div className="image">
+          <img src="joffee-image.png" />
+        </div>
+        <div className="image">
+          <img src="nexus-image.png" />
+        </div>
+      </div>
+    );
+}
