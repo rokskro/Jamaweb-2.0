178ba572224cad95bb6fad7598f50c125f2a1258
rox <rokskro@gmail.com>
2024-02-19 10:42:32 +0000

    css update

--- src/components/QuadContainer.jsx
+++ src/components/QuadContainer.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import '../css/QuadContainer.css';
 import HomeCard from './HomeCard';
 import ImageContainer from '../components/ImageContainer';
 
 export default function QuadContainer(){
     return(
         <div className="container">
-            <div className="card grey">
                 <hr className="clear"/>
                 <div className="row row-cols-1 row-cols-md-3 g-4">
                     <HomeCard title="Jammiest Deal of the Day">
                         {/* Content for card 1 */}
                     </HomeCard>
                     <HomeCard title="Jamazon Essentials">
-                        {ImageContainer}
+                        {<ImageContainer />}
                     </HomeCard>
                     <HomeCard title="Sign in for the best experience">
                         {/* Content for card 3 */}
@@ -20,7 +20,6 @@ export default function QuadContainer(){
                     <HomeCard title="Card title">
                         {/* Content for card 4 */}
                     </HomeCard>
-                </div>
             </div>
         </div>
     )
