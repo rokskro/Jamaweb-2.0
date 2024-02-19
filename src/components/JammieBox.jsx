249d89f55a9aee664e71a5eb51bc4a531a318ea6
rox <rokskro@gmail.com>
2024-02-19 12:05:18 +0000

    image fix

--- src/components/JammieBox.jsx
+++ src/components/JammieBox.jsx
@@ -1,10 +1,10 @@
 import JamImage from '/jam-image.png';
+import '../css/QuadContainer.css';
 
 export default function JammieBox(){
     return( 
         <div>
-            <h5 className="card-title">Jammiest Deal of the Day</h5>
-            <img src="jam-image.png" style="width: 370px;"/>
+            <img src={JamImage} className="img-width-370px"/>
             <p className="card-text"><span className="badge text-bg-danger">Up to 15% off</span> Ends in: </p>
             <p className="card-text">Up to 15% off on all banana & jam products</p>
         </div>
--- src/components/QuadContainer.jsx
+++ src/components/QuadContainer.jsx
@@ -3,3 +3,24 @@ import '../css/QuadContainer.css';
 import HomeCard from './HomeCard';
 import ImageContainer from '../components/ImageContainer';
 import JammieBox from '../components/JammieBox';
+
+export default function QuadContainer(){
+    return(
+        <div className="container">
+                <hr className="clear"/>
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <HomeCard title="Jammiest Deal of the Day">
+                        {<JammieBox />}
+                    </HomeCard>
+                    <HomeCard title="Jamazon Essentials">
+                        {<ImageContainer />}
+                    </HomeCard>
+                    <HomeCard title="Sign in for the best experience">
+                        <p className="font-size-13px">Enjoy the best Jamazon has to offer by tracking your orders, saving your basket and getting shopping reccomendations from FunkAI</p>
+                        <button className="btn amazonButton" type="submit">Login</button>
+                        <p className="card-text font-size-small">Dont have an account? Go register here</p>
+                    </HomeCard>
+            </div>
+        </div>
+    );
+}
